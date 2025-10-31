2dd666305e9c1fa2859dfe249f0e67cfebba8783
GritL1 <bingyanl@nycstudents.net>
2025-10-31 17:55:08 +0000

    a

--- src/main/java/com/example/project/StringProblems.java
+++ src/main/java/com/example/project/StringProblems.java
@@ -47,11 +47,14 @@ public class StringProblems{
     //deFront("a") -> "a"
     //deFront("ba") -> ""
     public String deFront(String s1){
-        if (s1.substring(0,1).equals("a")){
+        if (s1.substring(0,2).equals("ab")){
+            return s1.substring(0);
+        } else if (s1.substring(1,2).equals("b")){
             return s1.substring(1);
-        } 
-        s1.substring(3);
-        return "";
+        } else if (s1.substring(0,1).equals("a")){
+            return s1.substring(0,1) + s1.substring(2);
+        }
+        return s1.substring(2);
     }
 
     
@@ -63,7 +66,15 @@ public class StringProblems{
     // withoutX("Hxix") → "Hxi"
     // withoutX("") → ""
     public String withoutX(String s1){
-        return "";
+        if (s1.indexOf("x") != -1){
+            if (s1.substring(0, 1).equals("x")){
+                s1=s1.substring(1);
+            }
+            if (s1.substring(s1.length()-1).equals("x")){
+                s1=s1.substring(0,s1.length()-1);
+            }
+        }
+        return s1;
     }
 
     // Given a string str, if the string starts with "f" return "Fizz".
@@ -74,7 +85,16 @@ public class StringProblems{
     // fizzString("dib") → "Buzz"
     // fizzString("fib") → "FizzBuzz"
     public String fizzString(String s1){
-        return "";
+        if (s1.substring(0,1).equals("f")){
+            if (s1.substring(s1.length()-1).equals("b")){
+                return "FizzBuzz";
+            }
+            return "Fizz";
+        }
+        if (s1.substring(s1.length()-1).equals("b")){
+                return "Buzz";
+        }
+        return s1;
     }
 
     // Given an int n, return the string form of the number followed 
@@ -87,6 +107,15 @@ public class StringProblems{
     // fizzString2(2) → "2!"
     // fizzString2(3) → "Fizz!"
     public String fizzString2(int x){
-        return "";
+        if (x%3==0){
+            if (x%5==0){
+                return "FizzBuzz!";
+            }
+            return "Fizz!";
+        }
+        if (x%5==0){
+            return "Buzz!";
+        }
+        return x+"!";
     }
 }
